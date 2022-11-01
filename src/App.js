0506ee97f504223f56a1b50c812f03c00f6be608
Jasper Alblas <just_jasper@hotmail.com>
2022-11-01 19:55:48 +0100

    Search field input now used to call web service

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+import axios from "axios";
+
+const Input = ({ handleChange }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label htmlFor="search">find countries</label>
+      <br />
+      <input id="search" onChange={handleChange} />
     </div>
   );
+};
+
+const CountryInfo = ({ countries }) => {
+  return (
+    <div>
+      {countries.map((country) => (
+        <p key={country.cioc}>{country.name.common}</p>
+      ))}
+    </div>
+  )
 }
+
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    const url = searchQuery === '' ? 'https://restcountries.com/v3.1/all' : `https://restcountries.com/v3.1/name/${searchQuery}`
+    axios
+      .get(url)
+      .then((response) => {
+        console.log(response.data);
+        setCountries(response.data);
+      })
+      .catch((e) => console.log(e));
+  }, [searchQuery]);
+
+  const handleChange = (event) => {
+    setSearchQuery(event.target.value)
+  };
+
+  return (
+    <>
+      <Input handleChange={handleChange} />
+      <CountryInfo countries={countries} />
+    </>
+  );
+};
 
 export default App;

0c87fa7a772bdb63dade89da9705e2f1967ce359
Jasper Alblas <just_jasper@hotmail.com>
2022-11-02 20:10:16 +0100

    Country info now shown when one search result is shown

--- src/App.js
+++ src/App.js
@@ -5,33 +5,65 @@ import axios from "axios";
 const Input = ({ handleChange }) => {
   return (
     <div>
-      <label htmlFor="search">find countries</label>
+      <label htmlFor="search">Find countries</label>
       <br />
       <input id="search" onChange={handleChange} />
     </div>
   );
 };
 
-const CountryInfo = ({ countries }) => {
+const CountryList = ({ countries }) => {
   if (countries.length > 10) {
-    return (<p>GHAGA</p>)
+    return <p>Too many matches, specify another filter</p>;
   }
   return (
     <div>
-
       {countries.map((country) => (
         <p key={country.cca2}>{country.name.common}</p>
       ))}
     </div>
-  )
-}
+  );
+};
+
+const CountryDetails = ({ country }) => {
+  console.log(country.flags.png)
+  return (
+    <div>
+      <h2>{country.name.common}</h2>
+
+      <h3>capital</h3>
+      {country.capital}
+      <h3>area</h3>
+      {country.area}
+      <h3>languages</h3>
+
+      {Object.values(country.languages).map((value, index) => {
+        return (
+          <ul key={index}>
+            <li>{value}</li>
+          </ul>
+        );
+      })}
+
+      <h3>flag img</h3>
+      <img src={country.flags.png}></img>
+    </div>);
+};
 
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
 
+  const filteredCountries =
+    searchQuery.length !== 0
+      ? countries.filter((country) =>
+        country.name.common.toLowerCase().includes(searchQuery.toLowerCase())
+      )
+      : countries;
+  console.log(filteredCountries);
+
   useEffect(() => {
-    const url = 'https://restcountries.com/v3.1/all';
+    const url = "https://restcountries.com/v3.1/all";
     axios
       .get(url)
       .then((response) => {
@@ -42,14 +74,17 @@ const App = () => {
   }, []);
 
   const handleChange = (event) => {
-    setSearchQuery(event.target.value)
+    setSearchQuery(event.target.value);
   };
 
+  let content = filteredCountries.length === 1 ? <CountryDetails country={filteredCountries[0]} /> : <CountryList countries={filteredCountries} />
+
   return (
-    <>
+    <div>
+      <h1>Country info</h1>
       <Input handleChange={handleChange} />
-      <CountryInfo countries={countries} />
-    </>
+      {content}
+    </div>
   );
 };
 
